369199d75d1f7bd722e577bdf7000ad6dde4ec25
Ethan Collier <ethanacollier@icloud.com>
2024-09-17 20:34:31 -0600

    2024.09.17 phase0

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,15 +8,20 @@ package chess;
  */
 public class ChessPosition {
 
-    public ChessPosition(int row, int col) {
-    }
+  private final int row;
+  private final int col;
+
+  public ChessPosition(int row, int col) {
+    this.row=row;
+    this.col=col;
+  }
 
     /**
      * @return which row this position is in
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -24,6 +29,6 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
     }
 }
