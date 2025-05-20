19e6390cd045df4d50f58839df49dfde02d57007
Ethan Collier <ethanacollier@icloud.com>
2025-05-19 20:16:08 -0600

    Added functionality to the Server class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,13 @@
 package server;
 
+import handler.GameHandler;
+import handler.UserHandler;
+import service.ResponseException;
 import spark.*;
 
 public class Server {
+    private final UserHandler userHandler = new UserHandler();
+    private final GameHandler gameHandler = new GameHandler();
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,11 +15,26 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
+        Spark.post("/user", userHandler::register);
+        Spark.post("/session", userHandler::login);
+        Spark.delete("/session", userHandler::logout);
+        Spark.get("/game", gameHandler::listGames);
+        Spark.post("/game", gameHandler::createGame);
+        Spark.put("/game", gameHandler::joinGame);
+        Spark.exception(ResponseException.class, this::exceptionHandler);
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    private void exceptionHandler(ResponseException rE, Request req, Response res) {
+        res.status(rE.StatusCode());
+        res.body(rE.getMessage());
+    }
+
+    public int port() {
         return Spark.port();
     }
 
