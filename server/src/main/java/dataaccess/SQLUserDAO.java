ea19858019952bbbcfdcec4ce8369d1e6f22424b
Ethan Collier <ethanacollier@icloud.com>
2025-05-26 17:20:45 -0600

    Implemented first pass at SQLUserDAO

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -2,20 +2,53 @@ package dataaccess;
 
 import model.UserData;
 
+import java.sql.SQLException;
+
 public class SQLUserDAO implements UserDAO{
 
     @Override
     public UserData addUser(UserData user) throws DataAccessException {
-        return null;
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var statement = connection.prepareStatement("INSERT INTO users (username, password, email) VALUES (?, ?, ?)")) {
+                UserData previousUserData = getUserByUsername(user.username());
+                statement.setString(1, user.username());
+                statement.setString(2, user.password()); //hash password here?
+                statement.setString(3, user.email());
+                statement.executeUpdate();
+                return previousUserData;
+            }
+        }
+        catch (SQLException exception) { //not sure if it should be catching a DataAccessException instead
+            throw new DataAccessException("Failed to add user", exception);
+        }
     }
 
     @Override
     public UserData getUserByUsername(String username) throws DataAccessException {
-        return null;
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var statement = connection.prepareStatement("SELECT username, password, email FROM users WHERE username = ?")) {
+                statement.setString(1, username);
+                try (var result = statement.executeQuery()) {
+                    var password = result.getString("password");
+                    var email = result.getString("email");
+                    return new UserData(username, password, email);
+                }
+            }
+        }
+        catch (SQLException exception) {
+            throw new DataAccessException("Failed to get user", exception);
+        }
     }
 
     @Override
     public void clearUsers() throws DataAccessException {
-
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var statement = connection.prepareStatement("TRUNCATE user")) {
+                statement.executeUpdate();
+            }
+        }
+        catch (SQLException exception) {
+            throw new DataAccessException("Failed to clear users", exception);
+        }
     }
 }
